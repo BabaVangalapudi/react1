cb4e1b818ec3852ddbfa9d090d5c5e4028fb81ab
BabaVangalapudi <babavangalapudi72@gmail.com>
2024-03-10 08:33:19 +0530

    chnaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import NameRender from './Namerender';
-import Counter from './counter';
-import './app.css'
-import CommentsCont from './comment';
 
 function App() {
   return (
     <>
-     <CommentsCont/>
+    
     </>
   )
 }
